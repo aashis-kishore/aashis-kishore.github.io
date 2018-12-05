5c043cce456461ce2553d63e841b686ae3e06602
Aashis Kishore <aashiskishore@gmail.com>
2018-12-06 00:48:28 +0530

    [Test]: Embellish for no reason at all

--- static/js/script.js
+++ static/js/script.js
@@ -17,15 +17,32 @@ function test_backend_connectivity() {
     fetch('https://chiaroscuro-back-end.herokuapp.com/')
         .then(res => res.json())
         .then(res => {
-            console.log(res);
+            console.log(res.name);
 
             const welcome_text = document.querySelector('h1');
+            let name = res.name.toLowerCase();
+            let index = 0;
 
             if (welcome_text && res.name) {
-                welcome_text.innerText = `Welcome home, ${res.name}`;
+                welcome_text.innerText = 'welcome home, ';
+                setTimeout(requestAnimationFrame.bind(null, animate_name.bind(null, name, index, welcome_text)), 1000);
             } else {
-                welcome_text.innerText = 'Greetings, Unknown';
+                welcome_text.innerText = 'greetings, ';
+                setTimeout(requestAnimationFrame.bind(null, animate_name.bind(null, 'Unknown', index, welcome_text)), 1000);
             }
         })
         .catch(err => console.log(err));
+}
+
+function animate_name(name, index, welcome_text) {
+    if (index < name.length) {
+        if (name.charAt(index) !== ' ') {
+            welcome_text.innerText += name.charAt(index);
+            index++;
+        } else {
+            welcome_text.innerText += name.charAt(index) + name.charAt(index + 1);
+            index += 2;
+        }
+        requestAnimationFrame(animate_name.bind(null, name, index, welcome_text));
+    }
 }
